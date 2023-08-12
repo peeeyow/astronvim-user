90c16186aa614fe632da34623288ecbf3df4c9bb
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-12 16:33:17 +0800

    feat(mason): configure mason lsp and null-ls

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,36 +1,44 @@
--- customize mason plugins
 return {
-  -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "bashls",
+        "cssls",
+        "eslint",
+        "dockerls",
+        "emmet_ls",
+        "graphql",
+        "html",
+        "jsonls",
+        "prismals",
+        "pyright",
+        "lua_ls",
+        "tailwindcss",
+        "tsserver",
+        "yamlls",
       })
+      opts.automatic_installation = true
+      return opts
     end,
   },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
       })
     end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
     opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "python",
+        "black",
+        "flake8",
+        "prettierd",
+        "stylua"
       })
+      return opts
     end,
   },
 }
