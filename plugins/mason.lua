f45c94ad640b87eda84063b50739e28f7438f80f
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-12 16:53:48 +0800

    feat(null-ls): configure null-ls

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -26,19 +26,18 @@ return {
     "jay-babu/mason-null-ls.nvim",
     opts = function(_, opts)
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "black",
+        "flake8",
+        "prettierd",
       })
+      opts.automatic_installation = { exclude = { "mypy" } }
+      return opts
     end,
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "black",
-        "flake8",
-        "prettierd",
-        "stylua"
-      })
-      return opts
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
     end,
   },
 }
--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -1,17 +1,43 @@
 return {
   "jose-elias-alvarez/null-ls.nvim",
   opts = function(_, config)
-    -- config variable is the default configuration table for the setup function call
-    -- local null_ls = require "null-ls"
+    local null_ls = require "null-ls"
+    local formatting = null_ls.builtins.formatting
+    local diagnostics = null_ls.builtins.diagnostics
+    local codeactions = null_ls.builtins.code_actions
+    local default_diagnostic_format = "#{m}\n(#{c})"
 
-    -- Check supported formatters and linters
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
-      -- Set a formatter
-      -- null_ls.builtins.formatting.stylua,
-      -- null_ls.builtins.formatting.prettier,
+      formatting.black.with {
+        prefer_local = true,
+      },
+      formatting.djlint.with {
+        extra_args = { "--indent", "2" },
+        prefer_local = true,
+      },
+      formatting.isort.with {
+        prefer_local = true,
+      },
+      formatting.prettierd,
+      formatting.stylua,
+      diagnostics.djlint.with {
+        prefer_local = true,
+      },
+      diagnostics.flake8.with {
+        diagnostics_format = default_diagnostic_format,
+      },
+      diagnostics.hadolint.with {
+        diagnostics_format = default_diagnostic_format,
+      },
+      diagnostics.mypy.with {
+        diagnostics_format = default_diagnostic_format,
+        prefer_local = true,
+      },
+      diagnostics.yamllint,
+      diagnostics.commitlint,
+      diagnostics.tfsec,
+      codeactions.gitsigns,
     }
-    return config -- return final config table
+    return config
   end,
 }
