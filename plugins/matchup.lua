62d2d89f515350d2bf15364abba065bc2f66b03c
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-12 23:08:31 +0800

    feat(treesitter): configure rainbow and match up

---
+++ plugins/matchup.lua
@@ -0,0 +1,4 @@
+return {
+  "andymass/vim-matchup",
+  config = function() vim.g.matchup_matchparen_offscreen = { method = "popup" } end,
+}
--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,39 @@
+local playground = {
+  enable = true,
+  disable = {},
+  updatetime = 25,
+  persist_queries = false,
+  keybindings = {
+    toggle_query_editor = "o",
+    toggle_hl_groups = "i",
+    toggle_injected_languages = "t",
+    toggle_anonymous_nodes = "a",
+    toggle_language_display = "I",
+    focus_language = "f",
+    unfocus_language = "F",
+    update = "R",
+    goto_node = "<cr>",
+    show_help = "?",
+  },
+}
+
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2",
+    "nvim-treesitter/playground",
+    "andymass/vim-matchup",
+  },
   opts = function(_, opts)
-    --   opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-    --     -- "lua"
-    --   })
-    opts.ensure_installed = "all"
+    return require("astronvim.utils").extend_tbl(opts, {
+      ensure_installed = "all",
+      rainbow = {
+        enable = true,
+      },
+      playground = playground,
+      matchup = {
+        enable = true,
+      },
+    })
   end,
 }
