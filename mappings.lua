bdb0f16d8875674dd8a39b67d635c5a5dadadc21
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-19 22:10:42 +0800

    refactor(keymaps): add guard statements

--- mappings.lua
+++ mappings.lua
@@ -1,12 +1,6 @@
-local function delete_empty()
-  if vim.api.nvim_get_current_line():match "^%s*$" then
-    return [["_dd]]
-  else
-    return "dd"
-  end
-end
+local is_available = require("astronvim.utils").is_available
 
-return {
+local maps = {
   n = {
     ["<C-d>"] = { "<C-d>zz" },
     ["<C-u>"] = { "<C-u>zz" },
@@ -15,18 +9,17 @@ return {
     ["J"] = { "mzJ`z" },
     ["<M-j>"] = { ":m .+1<cr>==", desc = "Move line one step below" },
     ["<M-k>"] = { ":m .-2<cr>==", desc = "Move line one step above" },
-    ["dd"] = { delete_empty, expr = true },
-    ["<leader>h"] = { ":noh<cr>", desc = "Remove search highlight" },
-    ["<leader>H"] = {
+    ["dd"] = {
       function()
-        local wins = vim.api.nvim_tabpage_list_wins(0)
-        if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
-          vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+        if vim.api.nvim_get_current_line():match "^%s*$" then
+          return [["_dd]]
+        else
+          return "dd"
         end
-        require("alpha").start(false, require("alpha").default_config)
       end,
-      desc = "Home Screen",
+      expr = true,
     },
+    ["<leader>h"] = { ":noh<cr>", desc = "Remove search highlight" },
     ["<leader>/"] = false,
   },
   v = {
@@ -40,3 +33,25 @@ return {
     ["p"] = { "p:let @+=@0<cr>" },
   },
 }
+
+if is_available "alpha-nvim" then
+  maps.n["<leader>H"] = function()
+    local wins = vim.api.nvim_tabpage_list_wins(0)
+    if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
+      vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+    end
+    require("alpha").start(false, require("alpha").default_config)
+  end
+end
+
+if is_available "neo-tree.nvim" then
+  maps.n["<leader>w"] = function()
+    if vim.bo.filetype == "neo-tree" then
+      vim.cmd.wincmd "p"
+    else
+      vim.cmd.Neotree "focus"
+    end
+  end
+end
+
+return maps
