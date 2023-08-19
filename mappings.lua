ccb6f071c3e7852949a1986a8a57b873e8a32a6a
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-20 00:20:19 +0800

    feat(keymaps): add descriptions

--- mappings.lua
+++ mappings.lua
@@ -35,23 +35,29 @@ local maps = {
 }
 
 if is_available "alpha-nvim" then
-  maps.n["<leader>H"] = function()
-    local wins = vim.api.nvim_tabpage_list_wins(0)
-    if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
-      vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
-    end
-    require("alpha").start(false, require("alpha").default_config)
-  end
+  maps.n["<leader>H"] = {
+    function()
+      local wins = vim.api.nvim_tabpage_list_wins(0)
+      if #wins > 1 and vim.api.nvim_get_option_value("filetype", { win = wins[1] }) == "neo-tree" then
+        vim.fn.win_gotoid(wins[2]) -- go to non-neo-tree window to toggle alpha
+      end
+      require("alpha").start(false, require("alpha").default_config)
+    end,
+    desc = "Home Screen",
+  }
 end
 
 if is_available "neo-tree.nvim" then
-  maps.n["<leader>w"] = function()
-    if vim.bo.filetype == "neo-tree" then
-      vim.cmd.wincmd "p"
-    else
-      vim.cmd.Neotree "focus"
-    end
-  end
+  maps.n["<leader>w"] = {
+    function()
+      if vim.bo.filetype == "neo-tree" then
+        vim.cmd.wincmd "p"
+      else
+        vim.cmd.Neotree "focus"
+      end
+    end,
+    desc = "Toggle Explorer Focus",
+  }
 end
 
 return maps
