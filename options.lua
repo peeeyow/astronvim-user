7bdf880b60435cf2b420b0e95cb476ec4b80bda5
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-22 16:48:21 +0800

    fix(markdown): unset default

--- options.lua
+++ options.lua
@@ -11,6 +11,7 @@ return function(local_vim)
   local_vim.opt.wrap = true
   local_vim.g.resession_enabled = true
   local_vim.g.maplocalleader = [[\]]
+  local_vim.g.markdown_recommended_style = false
   local_vim.opt.shortmess = vim.opt.shortmess + "c"
   local_vim.opt.nrformats = vim.opt.nrformats + { "alpha", "octal", "hex" }
   local_vim.opt.whichwrap = vim.opt.whichwrap + "<>[]hl"
