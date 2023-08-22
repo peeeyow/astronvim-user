4313a94ddb8594294043f292ef53cb83c2f54b0b
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-08-22 17:08:15 +0800

    fix(obsidian-nvim): unset mappings to prevent gf error

--- plugins/obsidian.lua
+++ plugins/obsidian.lua
@@ -38,6 +38,8 @@ return {
     use_advanced_uri = true,
     finder = "telescope.nvim",
 
+    mappings = {},
+
     templates = {
       subdir = "templates",
       date_format = "%Y-%m-%d-%a",
@@ -45,10 +47,7 @@ return {
     },
 
     note_frontmatter_func = function(note)
-      -- This is equivalent to the default frontmatter function.
       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      -- `note.metadata` contains any manually added fields in the frontmatter.
-      -- So here we just make sure those fields are kept in the frontmatter.
       if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
         for k, v in pairs(note.metadata) do
           out[k] = v
@@ -57,8 +56,6 @@ return {
       return out
     end,
 
-    -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-    -- URL it will be ignored but you can customize this behavior here.
     follow_url_func = vim.ui.open or require("astronvim.utils").system_open,
   },
 }
