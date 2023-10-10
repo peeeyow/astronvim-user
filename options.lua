d799ecbc2319b687d55c51a06684166f59f4e4fc
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2023-10-10 23:47:35 +0800

    fix(ufo-nvim): prevent unnecessary folding

--- options.lua
+++ options.lua
@@ -15,5 +15,7 @@ return function(local_vim)
   local_vim.opt.shortmess = vim.opt.shortmess + "c"
   local_vim.opt.nrformats = vim.opt.nrformats + { "alpha", "octal", "hex" }
   local_vim.opt.whichwrap = vim.opt.whichwrap + "<>[]hl"
+  local_vim.opt.foldlevel = 99
+  local_vim.opt.foldlevelstart = -1
   return local_vim
 end
