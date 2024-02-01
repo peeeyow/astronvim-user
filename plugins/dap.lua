85062285e114e2ef42f14208ff049daee703f72e
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-02-02 00:10:20 +0800

    feat(nvim-dap-python): add post-mortem debug

--- plugins/dap.lua
+++ plugins/dap.lua
@@ -1,30 +1,29 @@
+local prefix = "<leader>d"
 return {
   {
     "mfussenegger/nvim-dap",
-    config = function()
-      require("dap.ext.vscode").load_launchjs()
-    end,
+    config = function() require("dap.ext.vscode").load_launchjs() end,
   },
   {
     "rcarriga/nvim-dap-ui",
     keys = {
       {
-        "<leader>dU",
+        prefix .. "U",
         function() require("dapui").open { reset = true } end,
         desc = "Reset dap UI",
       },
       {
-        "<leader>df",
+        prefix .. "f",
         function() require("dap-python").test_method() end,
         desc = "Test method",
       },
       {
-        "<leader>dx",
+        prefix .. "x",
         function() require("dap-python").test_class() end,
         desc = "Test Class",
       },
       {
-        "<leader>ds",
+        prefix .. "s",
         function() require("dap-python").debug_selection() end,
         desc = "Debug selection",
         mode = "v",
@@ -35,6 +34,13 @@ return {
     "mfussenegger/nvim-dap-python",
     dependencies = "mfussenegger/nvim-dap",
     ft = "python",
+    keys = {
+      {
+        prefix .. "e",
+        function() require("dap").set_exception_breakpoints { "raised", "uncaught" } end,
+        desc = "Add post mortem debugging",
+      },
+    },
     config = function(_, opts)
       local path = require("mason-registry").get_package("debugpy"):get_install_path() .. "/venv/bin/python"
       require("dap-python").setup(path, opts)
