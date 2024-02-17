9fd46b41acf767bd94958fbb02265b5c757dc398
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-02-17 14:15:52 +0800

    refactor(obsidian-nvim): use new vim api

--- plugins/obsidian.lua
+++ plugins/obsidian.lua
@@ -138,7 +138,7 @@ return {
     note_frontmatter_func = function(note)
       if note.title then note:add_alias(note.title) end
       local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-      if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+      if note.metadata ~= nil and vim.tbl_isempty(note.metadata) then
         for k, v in pairs(note.metadata) do
           out[k] = v
         end
