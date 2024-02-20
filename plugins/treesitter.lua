9d3e8d6e2207b3972d5d27818dcc2ff71681e83a
Pio Mendoza <69317890+peeeyow@users.noreply.github.com>
2024-02-20 21:20:18 +0800

    feat(treesitter): remove deprecated TSPlayground

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,33 +1,12 @@
-local playground = {
-  enable = true,
-  disable = {},
-  updatetime = 25,
-  persist_queries = false,
-  keybindings = {
-    toggle_query_editor = "o",
-    toggle_hl_groups = "i",
-    toggle_injected_languages = "t",
-    toggle_anonymous_nodes = "a",
-    toggle_language_display = "I",
-    focus_language = "f",
-    unfocus_language = "F",
-    update = "R",
-    goto_node = "<cr>",
-    show_help = "?",
-  },
-}
-
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
     "HiPhish/rainbow-delimiters.nvim",
-    "nvim-treesitter/playground",
     "andymass/vim-matchup",
   },
   opts = function(_, opts)
     return require("astronvim.utils").extend_tbl(opts, {
       ensure_installed = "all",
-      playground = playground,
       matchup = {
         enable = true,
       },
